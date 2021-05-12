1cf1a01532aafb9938688018e9bf29b1df7a8802
iulianstan <iulian1226@gmail.com>
2021-05-12 16:14:54 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,24 @@
-// Any code you will write for your website (in the future!) should go here
+// Select HTML elements
+
+const playerStatus = document.querySelector(".status");
+const resetButton = document.querySelector(".reset");
+const gridCell = document.querySelector(".grid");
+
+// Constant variables
+
+const xSymb = "Χ";
+const oSymb = "◯";
+
+// Function letter2Symb
+
+const letter2Symb = (letter) => (letter === "x" ? xSymb : oSymb);
+
+// Function playerWin
+
+const playerWin = (letter) => {
+  gameIsLive = false;
+  winner = letter;
+  if (winner === "x")
+    playerStatus.innerHTML = `Player ${letter2Symb(winner)} is the WINNER!`;
+  else playerStatus.innerHTML = `Player ${letter2Symb(winner)} is the WINNER!`;
+};
